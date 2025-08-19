8a22ca9e7f785cf6b26eb6e07bb04834ca1ab597
Gabriel <gabrielharimutobn@gmail.com>
2025-08-19 10:12:53 +0700

    Perbaiki build error

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,3 @@
-// src/firebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
@@ -7,19 +6,14 @@ const firebaseConfig = {
   apiKey: "AIzaSyDOBgGb6p9uN_ZQUO7CS6f5yu5NfeeQd9o",
   authDomain: "myportfoliochat-294d0.firebaseapp.com",
   projectId: "myportfoliochat-294d0",
-  storageBucket: "myportfoliochat-294d0.firebasestorage.appspot.com", // Ubah ke .appspot.com jika error
+  storageBucket: "myportfoliochat-294d0.appspot.com", // Perbaiki jika masih .firebasestorage.app
   messagingSenderId: "1028633746814",
   appId: "1:1028633746814:web:b83a01db14ccf55d55672f"
 };
 
-// Init Firebase
 const app = initializeApp(firebaseConfig);
-
-// Auth
 export const auth = getAuth(app);
 const provider = new GoogleAuthProvider();
 export const loginWithGoogle = () => signInWithPopup(auth, provider);
 export const logout = () => signOut(auth);
-
-// Firestore
 export const db = getFirestore(app);
